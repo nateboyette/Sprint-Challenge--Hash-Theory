d999317d4bf0cdc1c15e304d57193685a6f9eca7
Nate Boyette <nateboyette@icloud.com>
2019-06-07 17:10:16 -0700

    Update ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -3,153 +3,153 @@
 #include "hashtable.h"
 #include "ex1.h"
 
-LinkedPair *create_pair(int key, int value)
-{
-  LinkedPair *pair = malloc(sizeof(LinkedPair));
-  pair->key = key;
-  pair->value = value;
-  pair->next = NULL;
+// LinkedPair *create_pair(int key, int value)
+// {
+//   LinkedPair *pair = malloc(sizeof(LinkedPair));
+//   pair->key = key;
+//   pair->value = value;
+//   pair->next = NULL;
 
-  return pair;
-}
+//   return pair;
+// }
 
-// djb2 hash function
-unsigned int hash(unsigned int x, int max)
-{
-  x = ((x >> 16) ^ x) * 0x45d9f3b;
-  x = ((x >> 16) ^ x) * 0x45d9f3b;
-  x = (x >> 16) ^ x;
-  return x % max;
-}
+// // djb2 hash function
+// unsigned int hash(unsigned int x, int max)
+// {
+//   x = ((x >> 16) ^ x) * 0x45d9f3b;
+//   x = ((x >> 16) ^ x) * 0x45d9f3b;
+//   x = (x >> 16) ^ x;
+//   return x % max;
+// }
 
-void destroy_pair(LinkedPair *pair)
-{
-  if (pair != NULL)
-    free(pair);
-}
+// void destroy_pair(LinkedPair *pair)
+// {
+//   if (pair != NULL)
+//     free(pair);
+// }
 
-HashTable *create_hash_table(int capacity)
-{
-  HashTable *ht = malloc(sizeof(HashTable));
-  ht->capacity = capacity;
-  ht->storage = calloc(capacity, sizeof(LinkedPair *));
+// HashTable *create_hash_table(int capacity)
+// {
+//   HashTable *ht = malloc(sizeof(HashTable));
+//   ht->capacity = capacity;
+//   ht->storage = calloc(capacity, sizeof(LinkedPair *));
 
-  return ht;
-}
+//   return ht;
+// }
 
-void hash_table_insert(HashTable *ht, int key, int value)
-{
-  unsigned int index = hash(key, ht->capacity);
+// void hash_table_insert(HashTable *ht, int key, int value)
+// {
+//   unsigned int index = hash(key, ht->capacity);
 
-  LinkedPair *current_pair = ht->storage[index];
-  LinkedPair *last_pair;
+//   LinkedPair *current_pair = ht->storage[index];
+//   LinkedPair *last_pair;
 
-  while (current_pair != NULL && current_pair->key != key)
-  {
-    last_pair = current_pair;
-    current_pair = last_pair->next;
-  }
+//   while (current_pair != NULL && current_pair->key != key)
+//   {
+//     last_pair = current_pair;
+//     current_pair = last_pair->next;
+//   }
 
-  if (current_pair != NULL)
-  {
-    current_pair->value = value;
-  }
-  else
-  {
-    LinkedPair *new_pair = create_pair(key, value);
-    new_pair->next = ht->storage[index];
-    ht->storage[index] = new_pair;
-  }
-}
+//   if (current_pair != NULL)
+//   {
+//     current_pair->value = value;
+//   }
+//   else
+//   {
+//     LinkedPair *new_pair = create_pair(key, value);
+//     new_pair->next = ht->storage[index];
+//     ht->storage[index] = new_pair;
+//   }
+// }
 
-void hash_table_remove(HashTable *ht, int key)
-{
-  unsigned int index = hash(key, ht->capacity);
+// void hash_table_remove(HashTable *ht, int key)
+// {
+//   unsigned int index = hash(key, ht->capacity);
 
-  LinkedPair *current_pair = ht->storage[index];
-  LinkedPair *previous_pair = NULL;
+//   LinkedPair *current_pair = ht->storage[index];
+//   LinkedPair *previous_pair = NULL;
 
-  while (current_pair != NULL && current_pair->key != key)
-  {
-    previous_pair = current_pair;
-    current_pair = current_pair->next;
-  }
+//   while (current_pair != NULL && current_pair->key != key)
+//   {
+//     previous_pair = current_pair;
+//     current_pair = current_pair->next;
+//   }
 
-  if (current_pair == NULL)
-  {
-    fprintf(stderr, "Unable to remove entry with key: %d\n", key);
-  }
-  else
-  {
-    if (previous_pair == NULL)
-    { // Removing the first element in the Linked List
-      ht->storage[index] = current_pair->next;
-    }
-    else
-    {
-      previous_pair->next = current_pair->next;
-    }
+//   if (current_pair == NULL)
+//   {
+//     fprintf(stderr, "Unable to remove entry with key: %d\n", key);
+//   }
+//   else
+//   {
+//     if (previous_pair == NULL)
+//     { // Removing the first element in the Linked List
+//       ht->storage[index] = current_pair->next;
+//     }
+//     else
+//     {
+//       previous_pair->next = current_pair->next;
+//     }
 
-    destroy_pair(current_pair);
-  }
-}
+//     destroy_pair(current_pair);
+//   }
+// }
 
-int hash_table_retrieve(HashTable *ht, int key)
-{
-  unsigned int index = hash(key, ht->capacity);
-  LinkedPair *current_pair = ht->storage[index];
+// int hash_table_retrieve(HashTable *ht, int key)
+// {
+//   unsigned int index = hash(key, ht->capacity);
+//   LinkedPair *current_pair = ht->storage[index];
 
-  while (current_pair != NULL)
-  {
-    if (current_pair->key == key)
-    {
-      return current_pair->value;
-    }
+//   while (current_pair != NULL)
+//   {
+//     if (current_pair->key == key)
+//     {
+//       return current_pair->value;
+//     }
 
-    current_pair = current_pair->next;
-  }
+//     current_pair = current_pair->next;
+//   }
 
-  return -1;
-}
+//   return -1;
+// }
 
-void destroy_hash_table(HashTable *ht)
-{
-  LinkedPair *current_pair;
-  LinkedPair *pair_to_destroy;
+// void destroy_hash_table(HashTable *ht)
+// {
+//   LinkedPair *current_pair;
+//   LinkedPair *pair_to_destroy;
 
-  for (int i = 0; i < ht->capacity; i++)
-  {
-    current_pair = ht->storage[i];
-    while (current_pair != NULL)
-    {
-      pair_to_destroy = current_pair;
-      current_pair = current_pair->next;
-      destroy_pair(pair_to_destroy);
-    }
-  }
+//   for (int i = 0; i < ht->capacity; i++)
+//   {
+//     current_pair = ht->storage[i];
+//     while (current_pair != NULL)
+//     {
+//       pair_to_destroy = current_pair;
+//       current_pair = current_pair->next;
+//       destroy_pair(pair_to_destroy);
+//     }
+//   }
 
-  free(ht->storage);
-  free(ht);
-}
+//   free(ht->storage);
+//   free(ht);
+// }
 
-HashTable *hash_table_resize(HashTable *ht)
-{
-  HashTable *new_ht = create_hash_table(2 * ht->capacity);
+// HashTable *hash_table_resize(HashTable *ht)
+// {
+//   HashTable *new_ht = create_hash_table(2 * ht->capacity);
 
-  LinkedPair *current_pair;
-  for (int i = 0; i < ht->capacity; i++)
-  {
-    current_pair = ht->storage[i];
-    while (current_pair != NULL)
-    {
-      hash_table_insert(new_ht, current_pair->key, current_pair->value);
-      current_pair = current_pair->next;
-    }
-  }
+//   LinkedPair *current_pair;
+//   for (int i = 0; i < ht->capacity; i++)
+//   {
+//     current_pair = ht->storage[i];
+//     while (current_pair != NULL)
+//     {
+//       hash_table_insert(new_ht, current_pair->key, current_pair->value);
+//       current_pair = current_pair->next;
+//     }
+//   }
 
-  destroy_hash_table(ht);
-  return new_ht;
-}
+//   destroy_hash_table(ht);
+//   return new_ht;
+// }
 
 /*
 
@@ -212,28 +212,28 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
     // Only checking indices with a value
     if (ht->storage[i] != NULL)
     {
-      printf("HASH INDEX: %d\n", i);
+      // printf("HASH INDEX: %d\n", i);
 
-      printf("Hash KEY/WEIGHT: %d\n", ht->storage[i]->key);
+      // printf("Hash KEY/WEIGHT: %d\n", ht->storage[i]->key);
 
       // Since the weight is being stored as the key in the hash table
       // We need to find what keys will equal the limit
       // By subtracting the limit from the current key
       int weight_value = limit - ht->storage[i]->key;
-      printf("WEIGHT VALUE: %d\n", weight_value);
+      // printf("WEIGHT VALUE: %d\n", weight_value);
 
       // We then pass that key into hash table retrieve
       // Which will return the value of the key if it exists in the hash table
       // That value is the index of the weight in the weight array
       int weight_index = hash_table_retrieve(ht, weight_value);
-      printf("WEIGHT INDEX: %d\n", weight_index);
-      printf("LIMIT: %d\n", limit);
-      printf("*****\n\n");
+      // printf("WEIGHT INDEX: %d\n", weight_index);
+      // printf("LIMIT: %d\n", limit);
+      // printf("*****\n\n");
 
       // if the key + the weight value equal the limit AND the weight exists
       // in the hash table, assign the values to the answer struct and return
 
-            if ((ht->storage[i]->key + weight_value) == limit && weight_index != -1)
+      if ((ht->storage[i]->key + weight_value) == limit && weight_index != -1)
       {
 
         // printf("LIMIT = %d\n", limit);
